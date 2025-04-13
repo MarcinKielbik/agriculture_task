80d73328035bc077940d034d2980ee57092a3bc6
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-04-13 15:29:20 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+def maximal_square(matrix):
+    if not matrix or not matrix[0]:
+        return 0
+
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+
+
+    dp = [[0] * cols for _ in range(rows)]
+    max_side = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if matrix[i][j] == 1:
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                else:
+                    dp[i][j] = min(
+                        dp[i - 1][j],
+                        dp[i][j - 1],
+                        dp[i - 1][j - 1]
+                    ) + 1
+                max_side = max(max_side, dp[i][j])
+    return max_side * max_side
+
+
+matrix = [
+    [1, 0, 1, 0],
+    [1, 1, 1, 1],
+    [0, 1, 1, 1]
+]
+
+print(maximal_square(matrix))
+
